eb5a96e19fac8b1ea798b8e070c2408fe99279d5
Monirul Hasan <kmhasan@gmail.com>
2018-10-18 10:46:27 +0600

    initial commit

---
+++ assignment1/src/bd/edu/seu/fall2018ai/assignment1/Main.java
@@ -0,0 +1,23 @@
+package bd.edu.seu.fall2018ai.assignment1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("view/mainui.fxml"));
+        primaryStage.setTitle("Fall 2018 AI Assignment 1");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ assignment1/src/bd/edu/seu/fall2018ai/assignment1/controller/MainUiController.java
@@ -0,0 +1,4 @@
+package bd.edu.seu.fall2018ai.assignment1.controller;
+
+public class MainUiController {
+}
