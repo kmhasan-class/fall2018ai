ca9379e0ab860264f5c526986b18c52df9701398
Monirul Hasan <kmhasan@gmail.com>
2018-10-18 13:40:36 +0600

    added some shape classes

--- assignment1/src/main/java/bd/edu/seu/fall2018ai/assignment1/controller/MainUiController.java
+++ assignment1/src/main/java/bd/edu/seu/fall2018ai/assignment1/controller/MainUiController.java
@@ -5,19 +5,31 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainUiController implements Initializable {
-    @FXML private Canvas canvas;
+    //@FXML private Canvas canvas;
     @FXML private Label statusLabel;
+    @FXML private Pane drawingPane;
+    @FXML private ScrollPane scrollPane;
 
-    private GraphicsContext gc;
+//    private GraphicsContext gc;
     private final double MAJOR_TICK = 100;
     private final double MINOR_TICK = 20;
 
+    private void drawGridOnPane() {
+        Rectangle backgroundRectangle = new Rectangle(0, 0, drawingPane.getWidth(), drawingPane.getHeight());
+        Line line = new Line(0, 0, drawingPane.getWidth(), drawingPane.getHeight());
+        drawingPane.getChildren().addAll(line);
+    }
+/*
     private void drawGrid() {
         gc.setFill(Color.LIGHTGRAY);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -42,11 +54,14 @@ public class MainUiController implements Initializable {
         gc.strokeRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
     }
+*/
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        gc = canvas.getGraphicsContext2D();
+//        gc = canvas.getGraphicsContext2D();
+        scrollPane.setStyle("-fx-focus-color: transparent;");
         statusLabel.setText("Started the application");
-        drawGrid();
+        drawGridOnPane();
+//        drawGrid();
     }
 }
